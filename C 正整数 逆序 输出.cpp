74f3646f504069633c0085659589b7ca0d82586c
wanwangzi666 <62100759+wanwangzi666@users.noreply.github.com>
2022-03-31 13:25:23 +0800

    Create C 正整数 逆序 输出.cpp

--- C 正整数 逆序 输出.cpp
+++ C 正整数 逆序 输出.cpp
@@ -1,1 +1,12 @@
-﻿
+﻿#include<stdio.h>
+int main()
+{
+	int x, y, a, b, c;
+	scanf_s("%d", &x);
+	a = x / 100;
+	b = x / 10 - a * 10;
+	c = x - a * 100 - b * 10;
+	y = c * 100 + b * 10 + a;
+	printf("%d\n", y);
+	return 0;
+}
